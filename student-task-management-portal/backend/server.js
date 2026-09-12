bf3b9b3b65b3f996085052021c35cf3a595908c1
Harshitha-P2409 <harshitha.p.2006@gmail.com>
2026-09-12 13:13:19 +0530

    6th day morning

---
+++ student-task-management-portal/backend/server.js
@@ -0,0 +1,28 @@
+//bring express in Node.js
+const express=require("express")
+//create express app using what we imported
+const app=express();
+
+const tasks=[
+          {
+              id:1,
+              title:"Learn React",
+              description:"Understanding Components",
+              status: "Completed"
+          },
+          {
+              id:2,
+              title:"Learn JavaScript",
+              description:"Understanding Variables, Functions",
+              status: "Pending"
+          },
+      ];
+app.get("/api/tasks",(req,res)=>{res.json(tasks);});
+//our api route(testing)
+app.get("/",(req,res)=>{
+    res.send("Backend is working!!")
+});
+//start the server and listen to port 5000
+app.listen(5000,()=>{
+    console.log("Server us running on port 5000")
+});
