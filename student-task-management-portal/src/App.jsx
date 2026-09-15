a0d58a017a18a7b5c5dc65647568023669bdde1b
Harshitha-P2409 <harshitha.p.2006@gmail.com>
2026-09-15 09:44:41 +0530

    7th day

--- student-task-management-portal/src/App.jsx
+++ student-task-management-portal/src/App.jsx
@@ -9,26 +9,7 @@ import TaskDetails from "./components/TaskDetails";
 import { useState,useEffect} from "react";
 function App(){
 
-  const [tasks, setTasks] = useState([
-          {
-              id:1,
-              title:"Learn React",
-              description:"Understanding Components",
-              status: "Completed"
-          },
-          {
-              id:2,
-              title:"Learn JavaScript",
-              description:"Understanding Variables, Functions",
-              status: "Pending"
-          },
-          {   id:3,
-              title:"Learn MongoDB",
-              description:"Understanding Databases",
-              status: "Pending"
-          }
-          
-      ]);
+  const [tasks, setTasks] = useState([]);
       useEffect(()=>{
         fetch("http://localhost:5000/api/tasks").then((response)=>response.json()).then((data)=>{setTasks(data)});
       }, []);
